713afd3f7426e4a639bac05f45df285d0a66ca3b
K8OH8 <178042620+K80H8@users.noreply.github.com>
2025-02-28 17:32:30 -0800

    Working through problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let Celsius = (Fahrenheit - 32)/1.8;
+  return +Celsius.toFixed(1);
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let Fahrenheit = (Celsius * 1.8) + 32;
+  return +Fahrenheit.toFixed(1);
+
 };
 
 // Do not edit below this line
@@ -9,3 +16,18 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+//Celsius to fahrenheit
+// F = (C * 9/5) + 32
+// F - 32 = (C * 1.8)
+//(F - 32)/1.8 = C
+
+
+// ex: 100 = (c * 1.8) + 32
+// 68 = c * 1.8
+// c = 37.77778
+
+//Fahrenheit to Celsius
+// F = (C * 9/5) + 32
+//
